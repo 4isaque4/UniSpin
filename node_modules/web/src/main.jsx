3093ae468acebc1b2d59eadfa6cfaec18f7fcce1
Isaque Silva <isaquenun9@gmail.com>
2025-08-12 09:38:59 -0300

    Importação de estilos

--- node_modules/web/src/main.jsx
+++ node_modules/web/src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import App from './app/App.jsx'
+import './styles/global.css';
+import './styles/brand.css';   
+import './styles/App.css';     
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
