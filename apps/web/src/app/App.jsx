53389ddc451e9afb36daeae65ddf6fbf0fec46d3
Isaque Silva <isaquenun9@gmail.com>
2025-08-13 16:59:06 -0300

    feat(web): implementa roteamento principal da aplicação com React Router

--- apps/web/src/app/App.jsx
+++ apps/web/src/app/App.jsx
@@ -1,13 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "../components/Header.jsx";
-import Home from "./pages/Home.jsx";
 import Footer from "../components/Footer.jsx";
+
+/* pages */
+import Home from "./pages/Home.jsx";
+import Trilhas from "./pages/Trilhas.jsx";
+import Videos from "./pages/Videos.jsx";
+import Video from "./pages/Video.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 export default function App() {
   return (
-    <div className="app">
+    <BrowserRouter>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/trilhas" element={<Trilhas />} />
+        <Route path="/videos" element={<Videos />} />
+        <Route path="/videos/:id" element={<Video />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
