286b87b26419e682353ed0e5c1b0299c21751d3e
Isaque Silva <isaquenun9@gmail.com>
2025-08-22 15:45:27 -0300

    Corrigir import e uso da função API no Trilhas.jsx - usar apiFetch em vez de api

--- apps/web/src/app/routes/Trilhas.jsx
+++ apps/web/src/app/routes/Trilhas.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
-import { api } from "../../lib/api";
+import { apiFetch } from "../../lib/api";
 
 export default function Trilhas() {
   const [data, setData] = useState([]);
   const [err, setErr] = useState("");
 
   useEffect(() => {
-    api("/trilhas")
+    apiFetch("/trilhas")
+      .then((res) => res.json())
       .then(setData)
       .catch(e => setErr(e.message));
   }, []);
