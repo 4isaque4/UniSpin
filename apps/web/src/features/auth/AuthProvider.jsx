cd86e5c9259792645f2f1f4ebfdf9203314081b2
Isaque Silva <isaquenun9@gmail.com>
2025-08-22 15:29:57 -0300

    Configurando web

--- apps/web/src/features/auth/AuthProvider.jsx
+++ apps/web/src/features/auth/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../../lib/supabase";
-import { AuthCtx } from "./auth-context.js";
+import { AuthCtx } from "./auth-context.jsx";
 
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
